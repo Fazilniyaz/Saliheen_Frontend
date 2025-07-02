8a8e6186b831c5e6310d973ed26fed28fda96f75
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-07-02 12:24:19 +0530

    Update ConfirmOrder.js

--- src/components/cart/ConfirmOrder.js
+++ src/components/cart/ConfirmOrder.js
@@ -8,27 +8,29 @@ import Loader from "../layouts/Loader";
 import CheckoutSteps from "./CheckoutSteps";
 import axios from "axios";
 import RazorpayPayment from "../razorpay/RazorpayPayment";
+import { useContext } from "react";
+import { CartContext } from "../cart/cartContext";
 
 function ConfirmOrder() {
   const { shippingInfo, items: cartItems } = useSelector(
     (state) => state.cartState
   );
-  const [cartItemsFromDB, setCartItemsFromDB] = useState([]);
+  const [localCarts, setlocalCarts] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // Loader state
   const { user } = useSelector((state) => state.authState);
   const navigate = useNavigate();
+  const { localCart, addToLocalCart, removeFromLocalCart } =
+    useContext(CartContext);
+  console.log("localCart", localCart);
   console.log(user);
 
-  const products = cartItemsFromDB.map((item) => ({
-    _id: item.productId._id,
+  const products = localCart.map((item) => ({
+    _id: item.productId,
     quantity: item.quantity,
     stock: item.stock,
   }));
 
-  const itemsPrice = cartItemsFromDB.reduce(
-    (acc, item) => acc + item.finalPrice,
-    0
-  );
+  const itemsPrice = localCart.reduce((acc, item) => acc + item.finalPrice, 0);
   const shippingPrice = itemsPrice > 200 ? 0 : 25;
   const taxPrice = Number((0 * itemsPrice).toFixed(2)); // Ensure taxPrice is a number
   const totalPrice = Number((itemsPrice + shippingPrice + taxPrice).toFixed(2)); // Ensure totalPrice is calculated correctly
@@ -67,7 +69,7 @@ function ConfirmOrder() {
           `https://api.saliheenperfumes.com/api/v1/CartProductsOfSingleUser/${userId}`,
           { withCredentials: true }
         );
-        setCartItemsFromDB(data.cartItems);
+        setlocalCarts(data.cartItems);
       } catch (error) {
         console.error("Error fetching cart items:", error);
       } finally {
@@ -131,45 +133,46 @@ function ConfirmOrder() {
             >
               Your Cart Items:
             </h4>
-            {cartItemsFromDB.map((item) => (
-              <Fragment key={item._id}>
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginBottom: "1rem",
-                    color: "#fff",
-                  }}
-                >
-                  <img
-                    src={item?.productId?.images[0]?.image}
-                    alt={item.itemName}
+            {localCart.length > 0 &&
+              localCart.map((item) => (
+                <Fragment key={item.productId}>
+                  <div
                     style={{
-                      height: "45px",
-                      width: "65px",
-                      marginRight: "1rem",
-                      borderRadius: "5px",
-                    }}
-                  />
-                  <Link
-                    to={`/product/${item.productId._id}`}
-                    style={{
-                      color: "#a2682a",
-                      fontWeight: "bold",
-                      textDecoration: "none",
-                      flex: 1,
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: "1rem",
+                      color: "#fff",
                     }}
                   >
-                    {item.itemName}
-                  </Link>
-                  <p style={{ margin: 0 }}>
-                    {/* {item.quantity} x ${item.finalPrice} ={" "} */}
-                    <b>₹{item.finalPrice}</b>
-                  </p>
-                </div>
-                <hr style={{ borderColor: "#444" }} />
-              </Fragment>
-            ))}
+                    <img
+                      src={item?.productId?.images?.[0]?.image}
+                      alt={item.itemName}
+                      style={{
+                        height: "45px",
+                        width: "65px",
+                        marginRight: "1rem",
+                        borderRadius: "5px",
+                      }}
+                    />
+                    <Link
+                      to={`/product/${item.productId._id}`}
+                      style={{
+                        color: "#a2682a",
+                        fontWeight: "bold",
+                        textDecoration: "none",
+                        flex: 1,
+                      }}
+                    >
+                      {item.itemName}
+                    </Link>
+                    <p style={{ margin: 0 }}>
+                      {/* {item.quantity} x ${item.finalPrice} ={" "} */}
+                      <b>₹{item.finalPrice}</b>
+                    </p>
+                  </div>
+                  <hr style={{ borderColor: "#444" }} />
+                </Fragment>
+              ))}
           </div>
 
           <div className="col-12 col-lg-3 my-4">
@@ -237,6 +240,7 @@ function ConfirmOrder() {
                 taxPrice={taxPrice}
                 totalPrice={totalPrice}
                 products={products}
+                shippingInfo={shippingInfo}
               />
             </div>
           </div>

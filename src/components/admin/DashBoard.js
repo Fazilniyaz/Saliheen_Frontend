1734dd4c2638931a7010bb13efdf85b98a49f6b7
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-10-23 11:49:47 +0530

    design

--- src/components/admin/DashBoard.js
+++ src/components/admin/DashBoard.js
@@ -1,134 +1,3 @@
-// import Sidebar from "./SideBar";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
-// import { getAdminProducts } from "../../actions/productActions";
-// import { Link } from "react-router-dom";
-
-// export default function Dashboard() {
-//   const { products } = useSelector((state) => state.productsState);
-//   const dispatch = useDispatch();
-//   const { users = [] } = useSelector((state) => state.userState);
-//   let outOfStock = 0;
-
-//   if (products && products.length > 0) {
-//     products.forEach((product) => {
-//       if (product.stock === 0) {
-//         outOfStock = outOfStock + 1;
-//       }
-//     });
-//   }
-
-//   useEffect(() => {
-//     dispatch(getAdminProducts);
-//   }, []);
-
-//   return (
-//     <div className="row">
-//       <div className="col-12 col-md-2">
-//         <Sidebar />
-//       </div>
-//       <div className="col-12 col-md-10">
-//         <div className="container container-fluid">
-//           <div className="row">
-//             {/* <div className="col-12 col-md-2">
-//               <h4>Sidebar</h4>
-//             </div> */}
-
-//             <div className="col-12 col-md-10">
-//               <h1 className="my-4">Dashboard</h1>
-//               <div className="row pr-4">
-//                 <div className="col-xl-12 col-sm-12 mb-3">
-//                   <div className="card text-white bg-primary o-hidden h-100">
-//                     <div className="card-body">
-//                       <div className="text-center card-font-size">
-//                         Total Amount
-//                         <br /> <b>$3425</b>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="row pr-4">
-//                 <div className="col-xl-3 col-sm-6 mb-3">
-//                   <div className="card text-white bg-success o-hidden h-100">
-//                     <div className="card-body">
-//                       <div className="text-center card-font-size">
-//                         Products
-//                         <br /> <b>{products && products.length}</b>
-//                       </div>
-//                     </div>
-//                     <a
-//                       className="card-footer text-white clearfix small z-1"
-//                       to="/admin/products"
-//                     >
-//                       <span className="float-left">View Details</span>
-//                       <span className="float-right">
-//                         <i className="fa fa-angle-right"></i>
-//                       </span>
-//                     </a>
-//                   </div>
-//                 </div>
-
-//                 <div className="col-xl-3 col-sm-6 mb-3">
-//                   <div className="card text-white bg-danger o-hidden h-100">
-//                     <div className="card-body">
-//                       <div className="text-center card-font-size">
-//                         Orders
-//                         <br /> <b>345</b>
-//                       </div>
-//                     </div>
-//                     <a
-//                       className="card-footer text-white clearfix small z-1"
-//                       to="/admin/orders"
-//                     >
-//                       <span className="float-left">View Details</span>
-//                       <span className="float-right">
-//                         <i className="fa fa-angle-right"></i>
-//                       </span>
-//                     </a>
-//                   </div>
-//                 </div>
-
-//                 <div className="col-xl-3 col-sm-6 mb-3">
-//                   <div className="card text-white bg-info o-hidden h-100">
-//                     <div className="card-body">
-//                       <div className="text-center card-font-size">
-//                         <Link to="/admin/users">Users</Link>
-//                         <br /> <b>{users.length}</b>
-//                       </div>
-//                     </div>
-//                     <Link
-//                       className="card-footer text-white clearfix small z-1"
-//                       href="/admin/users"
-//                     >
-//                       <span className="float-left">View Details</span>
-//                       <span className="float-right">
-//                         <i className="fa fa-angle-right"></i>
-//                       </span>
-//                     </Link>
-//                   </div>
-//                 </div>
-
-//                 <div className="col-xl-3 col-sm-6 mb-3">
-//                   <div className="card text-white bg-warning o-hidden h-100">
-//                     <div className="card-body">
-//                       <div className="text-center card-font-size">
-//                         Out of Stock
-//                         <br /> <b>{outOfStock}</b>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useDispatch, useSelector } from "react-redux";
 import SideBar from "./SideBar";
 import { useEffect, useState } from "react";
@@ -192,108 +61,367 @@ export default function Dashboard() {
   }, [boolean]);
 
   return boolean ? (
-    <div className="container-fluid">
-      {" "}
-      {/* Full-width container */}
-      <div className="row">
-        <div className="col-12 col-md-2 px-0">
-          {" "}
-          {/* Remove padding */}
+    <div
+      className="container-fluid admin-dashboard"
+      style={{ padding: 0, margin: 0 }}
+    >
+      <div className="row g-0" style={{ margin: 0 }}>
+        {/* Sidebar Column */}
+        <div className="col-12 col-md-3 col-lg-2" style={{ padding: 0 }}>
           <SideBar />
         </div>
-        <div className="col-12 col-md-10">
-          <h1 className="my-4 headings">Dashboard</h1>
 
-          <div className="row pr-4">
-            <div className="col-xl-12 col-sm-12 mb-3">
-              <div className="card text-white bg-primary o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Total Amount
-                    <br />{" "}
-                    <Link to="/admin/salesReport">
-                      <b>${totalSales}</b>
-                    </Link>
-                  </div>
+        {/* Main Content Column - Full Width */}
+        <div
+          className="col-12 col-md-9 col-lg-10 dashboard-main"
+          style={{ padding: "20px", minHeight: "100vh" }}
+        >
+          {/* Dashboard Header */}
+          <div className="d-flex justify-content-between align-items-center mb-4">
+            <h1 className="gold-gradient-text mb-0">Dashboard</h1>
+            <div className="dashboard-date">
+              <span className="text-gold">
+                {new Date().toLocaleDateString()}
+              </span>
+              <span className="text-gold ms-3">
+                {new Date().toLocaleTimeString()}
+              </span>
+            </div>
+          </div>
+
+          {/* Total Sales Card - Full Width */}
+          <div className="row mb-4">
+            <div className="col-12">
+              <div className="gold-card h-100">
+                <div className="card-body text-center py-4">
+                  <i className="fas fa-dollar-sign stats-icon-large text-gold mb-3"></i>
+                  <div className="card-title-gold mb-2">Total Amount</div>
+                  <Link
+                    to="/admin/salesReport"
+                    className="gold-amount text-decoration-none"
+                  >
+                    <b>${totalSales?.toLocaleString() || 0}</b>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="row pr-4">
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-success o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Products
-                    <br /> <b>{products.length}</b>
+          {/* Stats Cards Grid */}
+          <div className="row g-3">
+            {/* Products Card */}
+            <div className="col-12 col-sm-6 col-xl-3">
+              <div className="stats-card products-card h-100">
+                <div className="card-body text-center py-4">
+                  <i className="fas fa-boxes stats-icon text-gold mb-3"></i>
+                  <div className="stats-value gold-gradient-text">
+                    {products.length}
                   </div>
+                  <div className="stats-label text-light mb-3">Products</div>
+                  <Link
+                    className="card-footer-btn w-100 text-decoration-none d-flex justify-content-between align-items-center py-2 px-3"
+                    to="/admin/products"
+                  >
+                    <span className="text-gold">View Details</span>
+                    <span className="text-gold">
+                      <i className="fas fa-arrow-right"></i>
+                    </span>
+                  </Link>
                 </div>
-                <a
-                  className="card-footer text-white clearfix small z-1"
-                  to="/admin/products"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </a>
               </div>
             </div>
 
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-danger o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Orders
-                    <br /> <b>{ordersCount}</b>
+            {/* Orders Card */}
+            <div className="col-12 col-sm-6 col-xl-3">
+              <div className="stats-card orders-card h-100">
+                <div className="card-body text-center py-4">
+                  <i className="fas fa-shopping-bag stats-icon text-gold mb-3"></i>
+                  <div className="stats-value gold-gradient-text">
+                    {ordersCount}
                   </div>
+                  <div className="stats-label text-light mb-3">Orders</div>
+                  <Link
+                    className="card-footer-btn w-100 text-decoration-none d-flex justify-content-between align-items-center py-2 px-3"
+                    to="/admin/orders"
+                  >
+                    <span className="text-gold">View Details</span>
+                    <span className="text-gold">
+                      <i className="fas fa-arrow-right"></i>
+                    </span>
+                  </Link>
                 </div>
-                <a
-                  className="card-footer text-white clearfix small z-1"
-                  to="/admin/orders"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </a>
               </div>
             </div>
 
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-info o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Users
-                    <br /> <b>{usersCount}</b>
+            {/* Users Card */}
+            <div className="col-12 col-sm-6 col-xl-3">
+              <div className="stats-card users-card h-100">
+                <div className="card-body text-center py-4">
+                  <i className="fas fa-users stats-icon text-gold mb-3"></i>
+                  <div className="stats-value gold-gradient-text">
+                    {usersCount}
                   </div>
+                  <div className="stats-label text-light mb-3">Users</div>
+                  <Link
+                    className="card-footer-btn w-100 text-decoration-none d-flex justify-content-between align-items-center py-2 px-3"
+                    to="/admin/users"
+                  >
+                    <span className="text-gold">View Details</span>
+                    <span className="text-gold">
+                      <i className="fas fa-arrow-right"></i>
+                    </span>
+                  </Link>
                 </div>
-                <a
-                  className="card-footer text-white clearfix small z-1"
-                  href="/admin/users"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </a>
               </div>
             </div>
 
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-warning o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Out of Stock
-                    <br /> <b>{outOfStock}</b>
+            {/* Out of Stock Card */}
+            <div className="col-12 col-sm-6 col-xl-3">
+              <div className="stats-card stock-card h-100">
+                <div className="card-body text-center py-4">
+                  <i className="fas fa-exclamation-triangle stats-icon text-gold mb-3"></i>
+                  <div className="stats-value gold-gradient-text">
+                    {outOfStock}
                   </div>
+                  <div className="stats-label text-light">Out of Stock</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      {/* Custom Styles */}
+      <style jsx>{`
+        .admin-dashboard {
+          background: linear-gradient(
+            135deg,
+            #0c0c0c 0%,
+            #1a1a1a 50%,
+            #0c0c0c 100%
+          );
+          min-height: 100vh;
+          color: white;
+          font-family: "Yantramanav", sans-serif;
+        }
+
+        .dashboard-main {
+          background: transparent;
+          width: 100%;
+        }
+
+        .gold-gradient-text {
+          background-image: repeating-linear-gradient(
+            to right,
+            #a2682a 0%,
+            #be8c3c 8%,
+            #be8c3c 18%,
+            #d3b15f 27%,
+            #faf0a0 35%,
+            #ffffc2 40%,
+            #faf0a0 50%,
+            #d3b15f 58%,
+            #be8c3c 67%,
+            #b17b32 77%,
+            #bb8332 83%,
+            #d4a245 88%,
+            #e1b453 93%,
+            #a4692a 100%
+          );
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          background-size: 200% auto;
+          font-weight: bold;
+          filter: drop-shadow(0 0 2px rgba(255, 200, 0, 0.5));
+          animation: MoveBackgroundPosition 6s ease-in-out infinite;
+        }
+
+        @keyframes MoveBackgroundPosition {
+          0% {
+            background-position: 0% center;
+          }
+          50% {
+            background-position: 100% center;
+          }
+          100% {
+            background-position: 0% center;
+          }
+        }
+
+        .text-gold {
+          color: #d4af37 !important;
+        }
+
+        .gold-card {
+          background: linear-gradient(135deg, #1a1a1a 0%, #2d2d2d 100%);
+          border: 1px solid #d4af37;
+          border-radius: 15px;
+          box-shadow: 0 8px 32px rgba(212, 175, 55, 0.2);
+          position: relative;
+          overflow: hidden;
+        }
+
+        .gold-card::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          height: 4px;
+          background: linear-gradient(
+            90deg,
+            #a2682a,
+            #be8c3c,
+            #d3b15f,
+            #faf0a0,
+            #d3b15f,
+            #be8c3c,
+            #a2682a
+          );
+        }
+
+        .card-title-gold {
+          font-size: 1.5rem;
+          color: #d4af37;
+          font-weight: 600;
+          margin-bottom: 1rem;
+        }
+
+        .gold-amount {
+          font-size: 3rem;
+          background-image: repeating-linear-gradient(
+            to right,
+            #a2682a,
+            #be8c3c,
+            #d3b15f,
+            #faf0a0,
+            #d3b15f,
+            #be8c3c,
+            #a2682a
+          );
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          text-decoration: none;
+          font-weight: bold;
+          display: block;
+        }
+
+        .stats-card {
+          background: linear-gradient(135deg, #1a1a1a 0%, #2d2d2d 100%);
+          border: 1px solid #333;
+          border-radius: 12px;
+          transition: all 0.3s ease;
+          position: relative;
+          overflow: hidden;
+          height: 100%;
+        }
+
+        .stats-card::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          height: 3px;
+          background: linear-gradient(
+            90deg,
+            #a2682a,
+            #be8c3c,
+            #d3b15f,
+            #faf0a0,
+            #d3b15f,
+            #be8c3c,
+            #a2682a
+          );
+          opacity: 0;
+          transition: opacity 0.3s ease;
+        }
+
+        .stats-card:hover {
+          transform: translateY(-5px);
+          border-color: #d4af37;
+          box-shadow: 0 10px 30px rgba(212, 175, 55, 0.3);
+        }
+
+        .stats-card:hover::before {
+          opacity: 1;
+        }
+
+        .stats-icon {
+          font-size: 2.5rem;
+          margin-bottom: 1rem;
+        }
+
+        .stats-icon-large {
+          font-size: 3rem;
+          margin-bottom: 1rem;
+        }
+
+        .stats-value {
+          font-size: 2.5rem;
+          font-weight: bold;
+          margin: 1rem 0;
+          line-height: 1;
+        }
+
+        .stats-label {
+          font-size: 1.1rem;
+          color: #ccc;
+          font-weight: 500;
+          margin-bottom: 1.5rem;
+        }
+
+        .card-footer-btn {
+          background: rgba(212, 175, 55, 0.1);
+          border: 1px solid rgba(212, 175, 55, 0.3);
+          border-radius: 8px;
+          transition: all 0.3s ease;
+          margin-top: auto;
+        }
+
+        .card-footer-btn:hover {
+          background: rgba(212, 175, 55, 0.2);
+          transform: translateY(-2px);
+        }
+
+        .dashboard-date {
+          font-size: 0.9rem;
+          color: #d4af37;
+        }
+
+        /* Mobile Responsive */
+        @media (max-width: 768px) {
+          .dashboard-main {
+            padding: 15px;
+          }
+
+          .gold-amount {
+            font-size: 2.2rem;
+          }
+
+          .stats-value {
+            font-size: 2rem;
+          }
+
+          .stats-icon {
+            font-size: 2rem;
+          }
+
+          .stats-icon-large {
+            font-size: 2.5rem;
+          }
+        }
+
+        @media (max-width: 576px) {
+          .gold-amount {
+            font-size: 1.8rem;
+          }
+
+          .stats-value {
+            font-size: 1.8rem;
+          }
+        }
+      `}</style>
     </div>
   ) : (
     <Loader />
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,20 +3,55 @@ import { Link } from "react-router-dom";
 
 function Product({ product }) {
   return (
-    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-      <div className="card p-3 rounded">
-        <img
-          className="card-img-top mx-auto"
-          src={product.images[0].image}
-          alt={product.name}
-        />
-        <div className="card-body d-flex flex-column">
-          <h5 className="card-title">
-            <Link to={`/product/${product._id}`}>
-              <h3>{product.name}</h3>
+    <div className="col-12 col-sm-6 col-md-6 col-lg-3 my-3 d-flex justify-content-center">
+      <div
+        className="card p-2 p-sm-3 rounded"
+        style={{ width: "100%", maxWidth: "280px" }}
+      >
+        {/* Fixed Image Container */}
+        <div
+          className="image-container mx-auto"
+          style={{
+            width: "100%",
+            height: "180px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            className="card-img-top"
+            src={product.images[0].image}
+            alt={product.name}
+            style={{
+              width: "auto",
+              height: "auto",
+              maxWidth: "100%",
+              maxHeight: "100%",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+
+        <div className="card-body d-flex flex-column p-1 p-sm-2">
+          {/* Product Name */}
+          <h5 className="card-title mb-2">
+            <Link
+              to={`/product/${product._id}`}
+              className="text-decoration-none"
+            >
+              <h6
+                className="mb-0 text-center"
+                style={{ fontSize: "clamp(14px, 4vw, 18px)" }}
+              >
+                {product.name}
+              </h6>
             </Link>
           </h5>
-          <div className="ratings mt-auto">
+
+          {/* Ratings */}
+          <div className="ratings mt-auto mb-2">
             <div className="rating-outer">
               <div
                 className="rating-inner"
@@ -25,13 +60,99 @@ function Product({ product }) {
                 }}
               ></div>
             </div>
-            <span id="no_of_reviews">{product.numOfReviews} Reviews</span>
+            <span id="no_of_reviews" style={{ fontSize: "12px" }}>
+              {product.numOfReviews} Reviews
+            </span>
           </div>
-          <p className="card-text">${product.price}</p>
-          <Link to={`/product/${product._id}`}>
-            <h3 id="view_btn" className="btn btn-block">
-              View Detail
-            </h3>
+
+          {/* Price */}
+          <p className="card-text text-center mb-2 fw-bold">${product.price}</p>
+
+          {/* Type Selection - Improved for mobile */}
+          <div className="mb-2">
+            <label className="form-label mb-1" style={{ fontSize: "14px" }}>
+              Type:
+            </label>
+            <div className="d-flex gap-1">
+              <button
+                className="btn btn-outline-primary btn-sm flex-fill"
+                style={{ fontSize: "12px", padding: "4px 8px" }}
+              >
+                Attar
+              </button>
+              <button
+                className="btn btn-outline-primary btn-sm flex-fill"
+                style={{ fontSize: "12px", padding: "4px 8px" }}
+              >
+                Perfume
+              </button>
+            </div>
+          </div>
+
+          {/* Quantity Selection - 2x2 Grid with better mobile handling */}
+          <div className="mb-2">
+            <label className="form-label mb-1" style={{ fontSize: "14px" }}>
+              Quantity:
+            </label>
+            <div className="row g-1">
+              <div className="col-6">
+                <button
+                  className="btn btn-outline-secondary w-100 btn-sm"
+                  style={{ fontSize: "11px", padding: "4px 2px" }}
+                >
+                  20ml
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  className="btn btn-outline-secondary w-100 btn-sm"
+                  style={{ fontSize: "11px", padding: "4px 2px" }}
+                >
+                  50ml
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  className="btn btn-outline-secondary w-100 btn-sm"
+                  style={{ fontSize: "11px", padding: "4px 2px" }}
+                >
+                  100ml
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  className="btn btn-outline-secondary w-100 btn-sm"
+                  style={{ fontSize: "11px", padding: "4px 2px" }}
+                >
+                  200ml
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* No. of Bottles - Improved for mobile */}
+          <div className="mb-3">
+            <label className="form-label mb-1" style={{ fontSize: "14px" }}>
+              No. of Bottles:
+            </label>
+            <input
+              type="number"
+              className="form-control form-control-sm"
+              min="1"
+              defaultValue="1"
+              style={{ fontSize: "14px" }}
+            />
+          </div>
+
+          {/* View Details Button */}
+          <Link to={`/product/${product._id}`} className="text-decoration-none">
+            <div
+              id="view_btn"
+              className="btn btn-primary btn-block w-100"
+              style={{ fontSize: "14px", padding: "6px 12px" }}
+            >
+              View Details
+            </div>
           </Link>
         </div>
       </div>
